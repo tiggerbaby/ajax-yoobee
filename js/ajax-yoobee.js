225214cc38bef60800d4cf981278e5770ffa9650
Shaoting Wu <alice.wu.nz@gmail.com>
2016-01-22 15:23:23 +1300

    use ajax to connect to yoobee.net

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -1,1 +1,22 @@
-alert('hi');
+$(document).ready(function(){
+	$('#note-form').submit( function( e ){
+		//prevent the form from submitting
+		e.preventDefault();
+
+		//Grab the note
+		if ($.trim ($('#note').val()) == '' ) {
+			return;
+	}
+
+		$.ajax({
+			url: 'http://alice.wu.yoobee.net.nz/ajax_yoobee/ajax-yoobee.php',
+			data:{ note: $('#note').val()},
+			success: function ( dataFromServer ){
+
+			},
+			error: function(){
+				alert('Cannot connect');
+			}
+		});
+	});
+});
