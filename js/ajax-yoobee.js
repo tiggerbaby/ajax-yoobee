e8fa26449102d3cc7732f44d6d0f9212555c1a19
Shaoting Wu <alice.wu.nz@gmail.com>
2016-01-26 12:28:38 +1300

    Show response from server in console

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
 		e.preventDefault();
 
 		//Grab the note
-		if ($.trim($('#note').val()) == '' ) {
+		if ($.trim( $('#note').val() ) == '' ) {
 			return;
-	}
+		}
 
 		$.ajax({
 			url: 'http://alice.wu.yoobee.net.nz/ajax_yoobee/ajax-yoobee.php',
 			data:{ note: $('#note').val()},
 			success: function ( dataFromServer ){
-
+     			console.log( dataFromServer );
 			},
 			error: function(){
 				alert('Cannot connect');
